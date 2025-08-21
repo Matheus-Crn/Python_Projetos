b0d94538c816b553d8cea969580faaa84604be77
Matheus Carneiro <matheusalc017@gmail.com>
2025-08-20 23:24:01 -0300

    Update
    print, escape character, letter F for floating point.

--- print.py
+++ print.py
@@ -10,5 +10,34 @@
 # canal = 'You Tube'
 # print(canal, '-', nome)
 
-nome = input('Digite seu nome: ')
-msg = ('')
+# print ('Imprime a mensagem e pula de linha')
+# print('Imprime a mensagem e continua na mesma linha.', end='') #end 
+# print(' Aqui,olha que legal')
+
+# nome = 'maria'
+# idade = 30
+
+# msg_formatada = 'O nome dela é {0} e ela tem {1} anos'.format(nome,idade)
+# print(msg_formatada)
+
+# nome = 'fabio'
+# peso = 73.50 
+# msg = f'olá, meu nome é {nome} e meu peso é {peso} kg'  #f string 
+# print(msg)
+
+# a = 10 
+# b = 5
+# res = f'A soma de {a} com {b} é igual a {a+b}'
+# print(res)
+
+# valor = 123.454322
+# print(f'O valor é {valor:.2f}') #indicando que quero duas casas decimais, e letra F para ponto flutuante
+
+# valor = 123.454322
+# print(f'O valor é \'{valor:.2f}\'') # caractere de escape, sairá de forma parecida com de string 
+
+
+# nome = 'matheus'
+# idade = 30
+# print(f'O nome\t{nome}\tIdade: {idade}')  #espaço com o escape \t 
+
